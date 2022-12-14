1a94b378eadde97318bee56068748a24b9c10f0b
jimenaRL <jimena@melix.net>
2022-12-14 12:24:44 +0100

    update data

--- data/sources/ALICE/data.py
+++ data/sources/ALICE/data.py
@@ -12,28 +12,32 @@ config = {
         "encoding": 'utf-8',
         "fns": {
             "OCDE_MISE-à_JOUR_2022DE---OCDE_ONU_GOV_Minist_Parlement_MàJ_2022.csv": "one_save",
+            "OCDE_MISE-à_JOUR_2022DE---OCDE_ECARTS_SALAIRE_MàJ_2022.csv": "one_save",
         },
     },
     "preprocesser": {
         'fns': {
             "OCDE_MISE-à_JOUR_2022DE---OCDE_ONU_GOV_Minist_Parlement_MàJ_2022.csv": ["remove_prop"],
+            "OCDE_MISE-à_JOUR_2022DE---OCDE_ECARTS_SALAIRE_MàJ_2022.csv": ["remove_prop"],
         },
         'rename': {
             'country': ['PAYS'],
             'year': ['DATE'],
             'indicator': ['DATA'],
-            'value': ['Part de femmes %'],
+            'value': ['Part de femmes %', 'Ecart %'],
         },
     },
     "mapper": {
         'fns': {
             "OCDE_MISE-à_JOUR_2022DE---OCDE_ONU_GOV_Minist_Parlement_MàJ_2022.csv": "proportion100",
+            "OCDE_MISE-à_JOUR_2022DE---OCDE_ECARTS_SALAIRE_MàJ_2022.csv": "proportion100",
         }
     },
     "filter": {
         'filter_indicator_path': None,
         'year': {
             "OCDE_MISE-à_JOUR_2022DE---OCDE_ONU_GOV_Minist_Parlement_MàJ_2022.csv": 2010,
+            "OCDE_MISE-à_JOUR_2022DE---OCDE_ECARTS_SALAIRE_MàJ_2022.csv": 2010,
         }
     },
     "translator": {
@@ -41,11 +45,13 @@ config = {
     "postprocesser": {
         'fns': {
             "OCDE_MISE-à_JOUR_2022DE---OCDE_ONU_GOV_Minist_Parlement_MàJ_2022.csv": "perc",
+            "OCDE_MISE-à_JOUR_2022DE---OCDE_ECARTS_SALAIRE_MàJ_2022.csv": "perc",
         }
     },
     "sorter": {
         'fns': {
             "OCDE_MISE-à_JOUR_2022DE---OCDE_ONU_GOV_Minist_Parlement_MàJ_2022.csv": "none",
+            "OCDE_MISE-à_JOUR_2022DE---OCDE_ECARTS_SALAIRE_MàJ_2022.csv": "none",
         }
     },
 }
