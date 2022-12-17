bb4aacd9eee2c115249d4a2d0e82cca79ef392f4
jimenaRL <jimena@melix.net>
2022-12-17 15:22:52 +0100

    update ALICE data

--- data/sources/ALICE/FUSION_Conditions de vie_18REG_Société_MAJ_2022/data.py
+++ data/sources/ALICE/FUSION_Conditions de vie_18REG_Société_MAJ_2022/data.py
@@ -11,26 +11,29 @@ config = {
         "header": 0,
         "encoding": 'utf-8',
         "fns": {
-            "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Données sociales_MàJ_2022.csv": "one_save",
-            "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_temps.csv": "one_save"
+            # "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Données sociales_MàJ_2022.csv": "one_save",
+            # "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_temps.csv": "one_save",
+            "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_parts.csv": "one_save"
         },
     },
     "preprocesser": {
         'fns': {
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Données sociales_MàJ_2022.csv": ["virg2point"],
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_temps.csv": [],
+            "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_parts.csv": ["virg2point"],
         },
         'rename': {
             'country': ['pays'],
             'year': ['annee'],
             'indicator': ['nom'],
-            'value': ['valeur ecart neg ou pos', 'différence en minutes'],
+            'value': ['valeur ecart neg ou pos', 'différence en minutes', 'valeur pp%'],
         },
     },
     "mapper": {
         'fns': {
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Données sociales_MàJ_2022.csv": "proportion100",
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_temps.csv": "diff_fm_minutes",
+            "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_parts.csv": "diffFH_50",
         }
     },
     "filter": {
@@ -38,6 +41,7 @@ config = {
         'year': {
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Données sociales_MàJ_2022.csv": 2010,
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_temps.csv": 2010,
+            "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_parts.csv": 2010,
         }
     },
     "translator": {
@@ -46,12 +50,14 @@ config = {
         'fns': {
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Données sociales_MàJ_2022.csv": "diff_perc",
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_temps.csv": "diff_minutes",
+            "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_parts.csv": "diff_pp",
         }
     },
     "sorter": {
         'fns': {
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Données sociales_MàJ_2022.csv": "none",
             "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_temps.csv": "none",
+            "FUSION_Conditions de vie_18REG_Société_MAJ_2022 - Conditions de vie_MàJ_2022_parts.csv": "none",
         }
     },
 }
