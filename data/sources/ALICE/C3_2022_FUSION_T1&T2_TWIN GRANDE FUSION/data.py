fbb18e191f9b6b59372c25ab823b6aab10eafc87
jimenaRL <jimena@melix.net>
2022-12-17 14:54:30 +0100

    update ALICE data

---
+++ data/sources/ALICE/C3_2022_FUSION_T1&T2_TWIN GRANDE FUSION/data.py
@@ -0,0 +1,53 @@
+import os
+import json
+import pandas as pd
+from murs_invisibles import Processer
+
+
+config = {
+    "base_path": os.path.dirname(os.path.realpath(__file__)),
+    "origin_language": "fr",
+    "io": {
+        "header": 0,
+        "encoding": 'utf-8',
+        "fns": {
+            "C3_2022_FUSION_T1&T2_TWIN GRANDE FUSION - Fusion_T2_ECarts_C3.csv": "one_save",
+        },
+    },
+    "preprocesser": {
+        'fns': {
+            "C3_2022_FUSION_T1&T2_TWIN GRANDE FUSION - Fusion_T2_ECarts_C3.csv": ["remove_prop", "virg2point"],
+        },
+        'rename': {
+            'country': ['pays'],
+            'year': ['annee'],
+            'indicator': ['nom'],
+            'value': ['valeur  %'],
+        },
+    },
+    "mapper": {
+        'fns': {
+            "C3_2022_FUSION_T1&T2_TWIN GRANDE FUSION - Fusion_T2_ECarts_C3.csv": "proportion100",
+            }
+    },
+    "filter": {
+        'filter_indicator_path': None,
+        'year': {
+            "C3_2022_FUSION_T1&T2_TWIN GRANDE FUSION - Fusion_T2_ECarts_C3.csv": 2010,
+        }
+    },
+    "translator": {
+    },
+    "postprocesser": {
+        'fns': {
+            "C3_2022_FUSION_T1&T2_TWIN GRANDE FUSION - Fusion_T2_ECarts_C3.csv": "perc",
+        }
+    },
+    "sorter": {
+        'fns': {
+            "C3_2022_FUSION_T1&T2_TWIN GRANDE FUSION - Fusion_T2_ECarts_C3.csv": "none",
+        }
+    },
+}
+
+Processer(config).process()
