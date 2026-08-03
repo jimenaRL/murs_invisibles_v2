080cafb275fa1dc4d73923afd5d3b273f6b22d0e
jimena <jimena@melix.net>
2026-08-03 16:32:43 +0200

    Add 2023 oecd entrepeneuariat data

---
+++ data/sources/oecd/entrepreneuriat/data.py
@@ -0,0 +1,54 @@
+import os
+from murs_invisibles import Processer
+
+
+file_dir = os.path.dirname(os.path.realpath(__file__))
+filter_indicator_path = os.path.join(file_dir, 'indicator_filter.txt')
+
+config = {
+    "base_path": os.path.dirname(os.path.realpath(__file__)),
+    "origin_language": "en",
+    "io": {
+        "header": 0,
+        "encoding": 'utf-8',
+        "fns": {
+            'OECD.DEV.NPG,DSD_GID@DF_GID_2023,+.RAPFR_AFS_PCT_1.PT_POP.....csv': 'sep_save'
+        },
+    },
+    "preprocesser": {
+        'fns': {
+            'OECD.DEV.NPG,DSD_GID@DF_GID_2023,+.RAPFR_AFS_PCT_1.PT_POP.....csv': ['get_wm_oecd_2026', 'diffFH']
+        },
+        'rename': {
+            'country': ['Reference area'],
+            'year': ['TIME_PERIOD'],
+            'indicator': ['Measure'],
+            'value': ['OBS_VALUE'],
+        },
+    },
+    "mapper": {
+        'fns': {
+            'OECD.DEV.NPG,DSD_GID@DF_GID_2023,+.RAPFR_AFS_PCT_1.PT_POP.....csv': 'diffFH_100'
+        }
+    },
+    "filter": {
+        'filter_indicator_path': filter_indicator_path,
+        'year': {
+            'OECD.DEV.NPG,DSD_GID@DF_GID_2023,+.RAPFR_AFS_PCT_1.PT_POP.....csv': 2000
+        }
+    },
+    "translator": {
+    },
+    "postprocesser": {
+        'fns': {
+            'OECD.DEV.NPG,DSD_GID@DF_GID_2023,+.RAPFR_AFS_PCT_1.PT_POP.....csv': 'diff_pp1'
+        }
+    },
+    "sorter": {
+        'fns': {
+            'OECD.DEV.NPG,DSD_GID@DF_GID_2023,+.RAPFR_AFS_PCT_1.PT_POP.....csv': 'date_country'
+        }
+    },
+}
+
+Processer(config).process()
